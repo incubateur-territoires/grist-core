11b39fb8415cea4c11187433f25f713f03f3cce2
Florent FAYOLLE <florent.fayolle@beta.gouv.fr>
2024-01-25 09:47:18 +0100

    WIP

--- load-tests/functions.js
+++ load-tests/functions.js
@@ -8,13 +8,23 @@ const logResponse = function (requestParams, response, context, ee, next) {
   return next();
 }
 
-const connectToWs = function (params, context, next) {
+const connectToWs = async function (params, context, next) {
   params.target = `${context.vars.wsTarget}`;
-  params.headers = {
-    'Authorization': `Bearer ${context.vars.bearer}`
+  const res = await fetch(`${context.vars.target}/o/docs/login?next=%2F`, {
+    method: 'GET',
+  });
+  params.options.headers = {
+    'Cookie': res.headers.getSetCookie().join('; '),
   };
   let reqId = 0;
   Object.defineProperty(context.vars, 'reqId', {
+    get: function () {
+      return reqId;
+    },
+    enumerable: true,
+    configurable: true
+  });
+  Object.defineProperty(context.vars, 'reqIdInc', {
     get: function () {
       return reqId++;
     },
@@ -23,4 +33,66 @@ const connectToWs = function (params, context, next) {
   });
   return next();
 };
-module.exports = { connectToWs, logHeaders, logResponse };
+
+
+const forgeScenario2ColumnsBody = function (requestParams, context, ee, next) {
+  const regularColumns = [
+    ...Array.from({ length: 19 }, (_, i) => {
+      return {
+        id: `column${i}`,
+        fields: {
+          label: `Column ${i}`,
+        }
+      }
+    }),
+    {
+      id: 'column19',
+      fields: {
+        label: 'Column 19',
+        type: "ChoiceList",
+        widgetOptions: JSON.stringify({
+          choices: Array.from({ length: 26 }, (_, i) => String.fromCharCode(0x61 + i)),
+        }),
+      }
+    }
+  ];
+  const getFormula = (i) => [
+    `str.upper($column${i})`,
+    `str.lower($column${i})`,
+    `str.capitalize($column${i})`,
+    `len($column${i})`,
+    `str.isalnum($column${i})`,
+    `str.isalpha($column${i})`,
+    `str.isdigit($column${i})`,
+    `str.islower($column${i})`,
+    `str.isupper($column${i})`,
+    `str.join('', reversed($column${i}))`,
+    `str.lstrip($column${i})`,
+    `str.rstrip($column${i})`,
+    `str.replace($column${i}, 'a', 'b')`,
+    `str.split($column${i}, ' ')`,
+    `str.startswith($column${i}, 'a')`,
+    `str.endswith($column${i}, 'a')`,
+    `str.swapcase($column${i})`,
+    `str.encode($column${i}, 'utf-8')`,
+    `str.index($column${i}, 'a')`,
+    `len(Table1.lookupRecords(column19=CONTAINS('a')))`,
+  ][i];
+  const formulaColumns = Array.from({ length: 20 }, (_, i) => {
+    return {
+      id: `formula${i}`,
+      fields: {
+        label: `Formula ${i}`,
+        isFormula: true,
+        formula: getFormula(i),
+        colId: `Formula${i}`,
+      }
+    };
+  });
+  requestParams.json = {
+    "columns": [...regularColumns, ...formulaColumns],
+  };
+  return next();
+}
+
+module.exports = { connectToWs, logHeaders, logResponse, forgeScenario2ColumnsBody };
